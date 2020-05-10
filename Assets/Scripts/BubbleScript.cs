e5232d4ff3ebd4b6e04e8b28ad2fb241d5adf7c6
Joao Leite <jopamoleite@gmail.com>
2020-05-10 20:08:39 +0100

    Added bubble

---
+++ Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleScript : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+    private int _bubbleValue = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = sprites[_bubbleValue-1]; 
+    }
+
+    void OnMouseDown(){
+        _bubbleValue = _bubbleValue % 4 + 1;   
+    }  
+}
---
+++ Assets/Scripts/CreateBubbles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBubbles : MonoBehaviour
+{
+    Vector2 gridSize;
+    GameObject[][] gridOfGameObjects;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        gridSize = new Vector2(5, 6);
+         gridOfGameObjects = new GameObject[(int)gridSize.x][];
+         for (int x = 0; x < gridSize.x; x++)
+         {
+             gridOfGameObjects[x] = new GameObject[(int)gridSize.y];
+             for (int y = 0; y < gridSize.y; y++)
+             {
+                 GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                 // manipulate gameobject here
+                 go.transform.position = new Vector3(x*2,y*2,0);
+                 gridOfGameObjects[x][y] = go;
+             }
+         }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
