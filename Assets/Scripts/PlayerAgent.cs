c8b8a2471b4841fb51ffdc202a98becba8e05dd2
Marcia Isabel Reis Teixeira <up201706065@fe.up.pt>
2020-05-25 16:57:07 +0100

    Added Behavior Parameters to Agent

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -72,26 +72,8 @@ public class PlayerAgent : Agent
 
     }
 
-    public override float[] Heuristic()
-    {
-        var action = new float[2];
-        action[0] = Input.GetAxis("Horizontal");
-        action[1] = Input.GetAxis("Vertical");
-        return action;
-    }
-
     public override void AgentReset()
     {
-        if(transform.position.y <0)
-        {
-            //if agent fell, zero its momentum
-            rBody.angularVelocity = Vector3.zero;
-            rBody.velocity = Vector3.zero;
-            transform.position = new Vector3(0, 0.5f, 0);
-        }
-
-        //move the target to a new spot
-        Target.position = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
     }
 
 }
